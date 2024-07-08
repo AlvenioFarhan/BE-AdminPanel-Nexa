3d91b1fbcb86db8936ddab09dfa70abd6c285fdc
Alvenio Farhan Prayogo <alveniofarhan@gmail.com>
2024-07-08 15:16:02 +0700

    Update Tampilan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+// Route::get('/form-transaksi', [TransaksiController::class, 'showForm'])->name('form-transaksi');
+
+Route::get('/form-transaksi', function () {
+    return view('form-transaksi');
+});
